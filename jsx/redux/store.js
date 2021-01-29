f8e0a2a6a59fe74fd2b33ef771253e954292f2da
Andy Situ <and.situ@gmail.com>
2021-01-28 22:04:03 -0800

    Add redux/store.js

---
+++ jsx/redux/store.js
@@ -0,0 +1,48 @@
+import { configureStore } from '@reduxjs/toolkit'
+import update from 'immutability-helper';
+
+
+const initialState = {
+  todo_list: [],
+};
+
+
+/***
+ * NOTE : TRY TO RETURN ONLY A COMPONENT OF STATE
+ * RATHER THAN COPY THE ENTIRE THING
+ */
+function reducer(state=initialState, action) {
+  if (action.type == "load_todos") {
+    return {
+      ...state,
+      todo_list: action.payload,
+    };
+  } else if (action.type == "change_todo_name") {
+    let new_state = {...state};
+    new_state.todo_list = [...state.todo_list];
+    new_state.todo_list[action.payload.index] = action.payload.name;
+    return new_state;
+  } else if (action.type == "add_todo") {
+    let new_state = {...state};
+    new_state.todo_list = [...state.todo_list];
+    new_state.todo_list.push(action.payload);
+    return new_state;
+  } else if (action.type == "add_task") {
+    let new_state = {...state};
+    new_state.todo_list[action.payload.index].tasks = [...state.todo_list[action.payload.index].tasks];
+    new_state.todo_list[action.payload.index].tasks.unshift(action.payload.data);
+    return new_state;
+  } else if (action.type == "delete_todo") {
+    let new_state = {...state};
+    new_state.todo_list = [...state.todo_list];
+    new_state.todo_list.splice(action.payload.index, 1);
+    return new_state;
+  } else if (action.type == "change_task_name") {
+    let new_state = {...state};
+    new_state.todo_list[action.payload.todo_index].tasks = 
+      [...state.todo_list[action.payload.todo_index].tasks ];
+    new_state.todo_list[action.payload.todo_index].tasks[action.payload.index]
+      = action.payload.name;
+    return new_state;
+  }
+}
