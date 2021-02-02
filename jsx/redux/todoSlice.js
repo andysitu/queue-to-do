c58a39e987b15b0212e025009f9ee4e11e3b6f77
Andy Situ <and.situ@gmail.com>
2021-02-01 23:49:01 -0800

    Update todoSlice.js

--- jsx/redux/todoSlice.js
+++ jsx/redux/todoSlice.js
@@ -32,44 +32,15 @@ export const todoSlice = createSlice({
       state.test += action.payload;
     },
     setTodo: (state, action) => {
-      console.log(action);
       state.todo_list = action.payload;
     },
   }
 });
 
-export const { increment, decrement, incrementByAmount } = todoSlice.actions
+export const { increment, decrement, incrementByAmount, setTodo } = todoSlice.actions
 
 export default todoSlice.reducer
 
 export const selectTest = state => state.todo.test;
 
 export const selectTodoList = state => state.todo.todo_list;
-
-export const loadToDo = () => {
-  return (dispatch, getState) => {
-    ipcRenderer.invoke("get-todo", (event, data) => {
-      let todo_list = [], 
-        todo_map = {},
-        index, todo;
-      
-      for (let i=0; i<data.length; i++) {
-        if (!(data[i].todo_id in todo_map)) {
-          todo_map[data[i].todo_id] = todo_list.length;
-          todo = extract_data_to_todo(data[i]);
-          if (data[i].task_id !== null) {
-            todo.tasks.push(extract_data_to_task(data[i]));
-          }
-
-          todo_list.push(todo);
-        } else {
-          if (data[i].task_id !== null) {
-            index = todo_map[data[i].todo_id];
-            todo_list[index].tasks.push(extract_data_to_task(data[i]));
-          }
-        }
-      }
-      dispatch(setTodo(todo_list));
-    });
-  }
-};
