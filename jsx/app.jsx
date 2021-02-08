68e3d08eb169c0f1119bd442ae5167209a1628fc
Andy Situ <and.situ@gmail.com>
2021-02-07 22:24:06 -0800

    For App, move Todo code to TodoConntainer

--- jsx/app.jsx
+++ jsx/app.jsx
@@ -1,4 +1,5 @@
 import { ModalMenu } from "./modalmenu.js"
+import { TodoContainer } from "./TodoContainer.js"
 
 const React = require('react');
 const {useState, useEffect} = require('react');
@@ -6,18 +7,13 @@ const ReactDom = require('react-dom');
 const {ipcRenderer, remote} = require('electron');
 const { Menu, MenuItem } = remote;
 
-import { TaskRow } from "./TaskRow.js"
-
 const { useSelector, useDispatch } = require('react-redux')
 import * as todoSlice from './redux/todoSlice.js'
 
 function App(props) {
   const dispatch = useDispatch();
-
-  const timer_interval = 700;
-  let todo_name_timer = null;
+  
   const todo_list = useSelector(todoSlice.selectTodoList);
-  console.log(todo_list);
 
   let modalmenu = React.createRef();
 
@@ -32,89 +28,14 @@ function App(props) {
     });
   };
 
-  const onClick_delete_todo = (e) => {
-    const id = e.target.getAttribute("todo_id"),
-          index = e.target.getAttribute("index");
-    const result = window.confirm(`Are you sure you want to delete todo ${todo_list[index].todo_name}?`);
-    if (result)  {
-      ipcRenderer.send("delete-todo", {todo_id: id});
-      ipcRenderer.once("delete-todo", (event, data) => {
-        dispatch(todoSlice.deleteTodo({ index:index}));
-      });
-    }
-  };
-
-  const onChange_todo_name_timer = (e) => {
-    const id = e.target.getAttribute("id"),
-          new_name = e.target.value,
-          index = parseInt(e.target.getAttribute("index"));
-
-    clearTimeout(todo_name_timer);
-
-    dispatch(todoSlice.editTodo(
-      { index:index, property: "name", value: new_name}));
-
-    todo_name_timer = setTimeout(() => {
-      var data = {
-        todo_id: id,
-        property: "name",
-        value: new_name,
-      };
-      ipcRenderer.send("edit-todo", data);
-    }, timer_interval);
-  };
-
-  const onClick_create_task = (e) => {
-    const todo_id = e.target.getAttribute("todo_id"),
-          index = e.target.getAttribute("index");
-    modalmenu.current.show_menu(
-      "create_task",
-      (data) => {
-        data.todo_id = todo_id;
-        ipcRenderer.send("create-task", data);
-        ipcRenderer.once("create-task",  (event, return_data) => {
-          dispatch(todoSlice.addTask({
-            task: props.extract_data_to_task(return_data),
-            index: index,
-          }));
-        });
-      }
-    )
-  };
-
   let create_todos = () => {
     return (
       todo_list.map((todo, todo_index)=> {
         return (
-          <div key={todo.todo_id} 
-          // onContextMenu={this.onContextMenu_todo}
-          >
-            <div>
-              <input type="text" 
-                value={todo.todo_name}
-                id={todo.todo_id} index={todo_index}
-                onChange={onChange_todo_name_timer}
-                ></input>
-              <button type="button"
-                todo_id={todo.todo_id} index={todo_index}
-                onClick={onClick_create_task}
-              >+</button>
-              <button type="button"
-                todo_id={todo.todo_id} index={todo_index}
-                onClick={onClick_delete_todo}
-              >
-                x
-              </button>
-            </div>
-            <div>
-              <ul>
-                {todo.tasks.map((task, task_index) => {
-                  return (
-                  <TaskRow key={task.task_id} task_index={task_index} todo_index={todo_index} />);
-                })}
-              </ul>
-            </div>
-          </div>);
+          <TodoContainer key={todo.todo_id}
+            extract_data_to_task={props.extract_data_to_task}
+            modalmenu={modalmenu}
+            todo_index={todo_index}/>);
       })
     );
   }
