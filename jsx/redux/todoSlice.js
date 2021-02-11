0ba4446c030bb47ae700965f048bd22efc524ce6
Andy Situ <and.situ@gmail.com>
2021-02-10 22:07:06 -0800

    Remove unused extract & test state from todoSlice.js

--- jsx/redux/todoSlice.js
+++ jsx/redux/todoSlice.js
@@ -1,20 +1,6 @@
 const { createSlice } = require('@reduxjs/toolkit');
 const {ipcRenderer} = require('electron');
 
-const extract_data_to_todo = (data) => {
-  return {
-    todo_id: data.todo_id,
-    todo_name: data.todo_name,
-    tasks: [],
-  }
-}
-const extract_data_to_task = (data) => {
-  return {
-    task_name: data.task_name,
-    task_id: data.task_id,
-  }
-}
-
 export const todoSlice = createSlice({
   name: "todo",
   initialState: {
@@ -86,7 +72,5 @@ export const { setTodo, deleteTodo, addTodo, editTodo,
               } = todoSlice.actions
 
 export default todoSlice.reducer
-
-export const selectTest = state => state.todo.test;
 
 export const selectTodoList = state => state.todo.todo_list;
