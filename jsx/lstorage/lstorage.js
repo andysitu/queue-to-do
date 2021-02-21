c6296728bcf1bead5c6eb26d385d327fe38c545e
Andy Situ <and.situ@gmail.com>
2021-02-20 17:34:30 -0800

    Update lstorage.js

--- jsx/lstorage/lstorage.js
+++ jsx/lstorage/lstorage.js
@@ -1,9 +1,11 @@
-class LStor {
-  getMultipleTasks(task_id) {
-
+const lstorage = {
+  multipleTasksIds_key: "multipleTasksIds",
+  getMultipleTasks() {
+    const ids = JSON.parse(localStorage.getItem(this.multipleTasksIds_key));
+    if (ids == null) return [];
+    return ids;
+  },
+  saveMultipleTasks(task_ids) {
+    localStorage.setItem(this.multipleTasksIds_key, JSON.parse(task_ids))
   }
-
-  saveMultipleTasks(task_id) {
-
-  }
-}
+};
