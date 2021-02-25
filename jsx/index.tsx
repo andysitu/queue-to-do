6db4bfc54d180c5de4421dee2f2c5b3533e57590
Andy Situ <and.situ@gmail.com>
2021-02-24 23:02:00 -0800

    Move loading data to redux state into function in index.tsx

--- jsx/index.tsx
+++ jsx/index.tsx
@@ -33,72 +33,76 @@ function extract_data_to_task(data) {
   }
 }
 
+function loadData(data) {
+  let new_list = [], 
+      todo_map = {},
+      task_dict = {},
+      incomplete_tasks = {},
+      complete_tasks = {},
+      task, todo, todo_id;
+  for (let i=0; i<data.length; i++) {
+    todo_id = data[i].todo_id;
+    if (!(todo_id in todo_map)) {
+      todo_map[todo_id] = new_list.length;
+      todo = extract_data_to_todo(data[i]);
+
+      task_dict[todo_id] = [];
+      incomplete_tasks[todo_id] = [];
+      complete_tasks[todo_id] = [];
+
+      new_list.push(todo);
+    }
+
+    if (data[i].task_id !== null) {
+      task = extract_data_to_task(data[i]);
+      task_dict[todo_id].push(task);
+      
+      if (task.task_done == 1) {
+        complete_tasks[todo_id].push(task);
+      } else {
+        incomplete_tasks[todo_id].push(task);
+      }
+    }
+  }
+
+  // Sort todos by todo_order, id if it doesn't exist
+  new_list.sort((a,b) => {
+    const aValue = (a.todo_order != null) ? a.todo_order : a.todo_id,
+          bValue = (b.todo_order != null) ? b.todo_order : b.todo_id;
+    return aValue - bValue;
+  });
+
+  function sortTask(a,b) {
+    const aValue = (a.task_order != null) ? a.task_order : a.task_id,
+      bValue = (b.task_order != null) ? b.task_order : b.task_id;
+    return aValue - bValue;
+  }
+
+  // Sort tasks in todos by task_order, id if it doesn't exist
+  for (let i=0; i< new_list.length; i++) {
+    todo_id = new_list[i].todo_id;
+    if (task_dict[todo_id].length > 1) {
+      task_dict[todo_id].sort(sortTask);
+    }
+    if (complete_tasks[todo_id].length > 1) {
+      complete_tasks[todo_id].sort(sortTask);
+    }
+    if (incomplete_tasks[todo_id].length > 1) {
+      incomplete_tasks[todo_id].sort(sortTask);
+    }
+  }
+  store.dispatch(todoSlice.setTodo(new_list));
+  store.dispatch(taskSlice.setTasks({
+    tasks_dict: task_dict,
+    incomplete_tasks: incomplete_tasks,
+    complete_tasks: complete_tasks
+  }));
+}
+
 let load_todo = () => {
   ipcRenderer.send("get-todo");
   ipcRenderer.once("get-todo", (event, data) => {
-    let new_list = [], 
-        todo_map = {},
-        task_dict = {},
-        incomplete_tasks = {},
-        complete_tasks = {},
-        task, todo, todo_id;
-    for (let i=0; i<data.length; i++) {
-      todo_id = data[i].todo_id;
-      if (!(todo_id in todo_map)) {
-        todo_map[todo_id] = new_list.length;
-        todo = extract_data_to_todo(data[i]);
-
-        task_dict[todo_id] = [];
-        incomplete_tasks[todo_id] = [];
-        complete_tasks[todo_id] = [];
-
-        new_list.push(todo);
-      }
-
-      if (data[i].task_id !== null) {
-        task = extract_data_to_task(data[i]);
-        task_dict[todo_id].push(task);
-        
-        if (task.task_done == 1) {
-          complete_tasks[todo_id].push(task);
-        } else {
-          incomplete_tasks[todo_id].push(task);
-        }
-      }
-    }
-
-    // Sort todos by todo_order, id if it doesn't exist
-    new_list.sort((a,b) => {
-      const aValue = (a.todo_order != null) ? a.todo_order : a.todo_id,
-            bValue = (b.todo_order != null) ? b.todo_order : b.todo_id;
-      return aValue - bValue;
-    });
-
-    function sortTask(a,b) {
-      const aValue = (a.task_order != null) ? a.task_order : a.task_id,
-        bValue = (b.task_order != null) ? b.task_order : b.task_id;
-      return aValue - bValue;
-    }
-
-    // Sort tasks in todos by task_order, id if it doesn't exist
-    for (let i=0; i< new_list.length; i++) {
-      todo_id = new_list[i].todo_id;
-      if (task_dict[todo_id].length > 1) {
-        task_dict[todo_id].sort(sortTask);
-      }
-      if (complete_tasks[todo_id].length > 1) {
-        complete_tasks[todo_id].sort(sortTask);
-      }
-      if (incomplete_tasks[todo_id].length > 1) {
-        incomplete_tasks[todo_id].sort(sortTask);
-      }
-    }
-    store.dispatch(todoSlice.setTodo(new_list));
-    store.dispatch(taskSlice.setTasks({
-      tasks_dict: task_dict,
-      incomplete_tasks: incomplete_tasks,
-      complete_tasks: complete_tasks
-    }));
+    loadData(data);    
 
     ReactDom.render(
       (<Provider store={store}>
