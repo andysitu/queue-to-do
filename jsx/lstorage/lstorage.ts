1408deda712816649d87c71add620ed93682ffde
Andy Situ <and.situ@gmail.com>
2021-02-24 23:14:07 -0800

    Update lstorage.ts

--- jsx/lstorage/lstorage.ts
+++ jsx/lstorage/lstorage.ts
@@ -13,6 +13,9 @@ const lstorage = {
     });
     localStorage.setItem(this.todo_settings_key, JSON.stringify(settings));
   },
+  saveSettings(settings) {
+    localStorage.setItem(this.todo_settings_key, JSON.stringify(settings));
+  }
   getSettings() {
     return JSON.parse(localStorage.getItem(this.todo_settings_key));
   }
